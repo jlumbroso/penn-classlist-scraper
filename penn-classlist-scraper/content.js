93e06c28a21fbd7f0800b0d4c5689e2a5e91a0b2
Jérémie Lumbroso <lumbroso@cs.princeton.edu>
2023-08-30 17:52:06 -0400

    Enhanced section data

--- penn-classlist-scraper/content.js
+++ penn-classlist-scraper/content.js
@@ -1,3 +1,24 @@
+// **************************************************************************
+// LOAD EXTERNAL RESOURCES
+// **************************************************************************
+
+let programData = [];
+let departmentCodes = [];
+
+async function loadProgramData() {
+    try {
+        const response = await fetch(chrome.runtime.getURL('programData.json'));
+        const data = await response.json();
+        
+        programData = data;
+        departmentCodes = programData.map(entry => entry.department);
+        return programData;  // Return the loaded data
+    } catch (error) {
+        console.error("Error loading programData:", error);
+        return null;  // Return null in case of an error
+    }
+}
+
 
 // **************************************************************************
 // HELPER METHODS
@@ -96,38 +117,165 @@ async function fetchAndEncodeImage(imageLink) {
     }
 }
 
+
+// Function to extract section information
+function extractSectionInfo(div) {
+    const sectionInfo = {};
+
+    // Extract section ID
+    const sectionMatch = div.textContent.match(/([A-Z0-9]+)\s+-\s+/);
+    if (sectionMatch) {
+        sectionInfo.sectionId = sectionMatch[1];
+    }
+
+    // Extract section title
+    const titleMatch = div.textContent.match(/-\s+([A-Za-z\s]+)\s+Enrl:/);
+    if (titleMatch) {
+        sectionInfo.title = titleMatch[1].trim();
+    }
+
+    // Extract enrollment
+    const enrollmentMatch = div.textContent.match(/Enrl:\s*(\d+)/);
+    if (enrollmentMatch) {
+        sectionInfo.enrollment = enrollmentMatch[1];
+    }
+
+    // Extract max enrollment
+    const maxEnrollmentMatch = div.textContent.match(/Mx Enrl:\s*(\d+)/);
+    if (maxEnrollmentMatch) {
+        sectionInfo.maxEnrollment = maxEnrollmentMatch[1];
+    }
+
+    // Extract mail using anchor tag with mailto
+    const mailAnchor = div.querySelector('a[href^="mailto:"]');
+    if (mailAnchor) {
+        sectionInfo.mail = mailAnchor.textContent;
+
+        // Extract year and term code from the mail
+        const termCodeMatch = mailAnchor.textContent.match(/(\d{6})@/);
+        if (termCodeMatch) {
+            sectionInfo.termCode = termCodeMatch[1];
+            sectionInfo.year = termCodeMatch[1].substring(0, 4);
+
+            const termTypeCode = termCodeMatch[1].substring(4);
+            switch (termTypeCode) {
+                case '10':
+                    sectionInfo.termType = 'Spring';
+                    break;
+                case '20':
+                    sectionInfo.termType = 'Summer';
+                    break;
+                case '30':
+                    sectionInfo.termType = 'Fall';
+                    break;
+            }
+        }
+    }
+
+    // Extract instructors
+    const instructorsDiv = div.querySelector('.ThemeGrid_Width8.ThemeGrid_MarginGutter');
+    if (instructorsDiv) {
+        sectionInfo.instructors = Array.from(instructorsDiv.querySelectorAll('span'))
+            .map(span => span.textContent.trim())
+            .filter(Boolean); // Filter out any empty strings
+    } else {
+        // Fallback to text parsing
+        const instructorsMatch = div.textContent.match(/Instructors:\s*([\s\S]+)/);
+        if (instructorsMatch) {
+            sectionInfo.instructors = instructorsMatch[1].split(',').map(instr => instr.trim());
+        }
+    }
+
+    // Construct sectionIdWithTerm using email address
+    if (sectionInfo.mail) {
+        const emailBreakdownMatch = sectionInfo.mail.match(/^([A-Z]+)-(\d{4})-(\d{3})-(\d{6})@/);
+        if (emailBreakdownMatch) {
+            sectionInfo.sectionIdWithTerm = emailBreakdownMatch[0].slice(0, -1);  // Remove trailing '@'
+        }
+    }
+
+    // If sectionIdWithTerm is not set using email, use programData.json
+    if (!sectionInfo.sectionIdWithTerm && sectionInfo.sectionId) {
+        for (const dept of departmentCodes) {
+            if (sectionInfo.sectionId.startsWith(dept)) {
+                const remainingPart = sectionInfo.sectionId.slice(dept.length);
+                sectionInfo.sectionIdWithTerm = `${dept}-${remainingPart}-${sectionInfo.termCode}`;
+                break;
+            }
+        }
+    }
+
+    // If sectionIdWithTerm is still not set, fall back to the original heuristic
+    if (!sectionInfo.sectionIdWithTerm && sectionInfo.sectionId && sectionInfo.termCode) {
+        // Heuristic: If sectionId is of the form ABC1234567, break it down into ABC-1234-567
+        const idBreakdownMatch = sectionInfo.sectionId.match(/^([A-Z]+)(\d{4})(\d{3})$/);
+        if (idBreakdownMatch) {
+            sectionInfo.sectionIdWithTerm = `${idBreakdownMatch[1]}-${idBreakdownMatch[2]}-${idBreakdownMatch[3]}-${sectionInfo.termCode}`;
+        } else {
+            // Fallback: Use the sectionId directly
+            sectionInfo.sectionIdWithTerm = `${sectionInfo.sectionId}-${sectionInfo.termCode}`;
+        }
+    }
+
+    return sectionInfo;
+}
+
+
+
 // Function to fetch all records
 async function fetchAllRecords() {
-    const tableRows = document.querySelectorAll('.pdfClassListEntry');
-    const allDataPromises = Array.from(tableRows).map(async (row, index) => {
-        try {
-            const data = extractDataFromRow(row);
-            console.log(`Row ${index + 1} DOM data extracted:`, data);  // Log the extracted data for each row
+    const tableRows = document.querySelectorAll('.pdfClassListEntry, .resultsWrappers.ClassListTable');
+    let currentSectionInfo = null;
+    const allData = [];
 
-            data.imageBlob = await fetchAndEncodeImage(data.imageLink);
-            return data;
+    // Ensure programData is loaded before proceeding
+    if (!programData.length) {
+        await loadProgramData();
+    }
+
+    for (const row of tableRows) {
+        try {
+            // Check if the row is a section information row
+            if (row.classList.contains('resultsWrappers') && row.classList.contains('ClassListTable')) {
+                currentSectionInfo = extractSectionInfo(row);
+                console.log('Detected section information row:', currentSectionInfo);
+                // We don't want to store section info rows in the same array as student rows
+                continue;
+            } 
+            // Check if the row is a student data row
+            else if (row.classList.contains('pdfClassListEntry')) {
+                const data = extractDataFromRow(row);
+                console.log('Extracted student row:', data);
+
+                // Inject the current section information
+                if (currentSectionInfo) {
+                    data.sectionInformation = currentSectionInfo;
+                }
+
+                data.imageBlob = await fetchAndEncodeImage(data.imageLink);
+                allData.push(data);
+            }
         } catch (error) {
             console.error('Error processing row:', row, error);
-            return null; // Return null for rows that encountered errors
         }
-    });
+    }
 
-    const allData = await Promise.all(allDataPromises);
-    return allData.filter(data => data !== null); // Filter out null entries
+    return allData;
 }
 
 
 // Generate a filename based on unique sections
 function generateFilename(data) {
-    const sections = new Set(data.map(entry => entry["Registered Section"]));
+    const sections = new Set(data.map(entry => {
+        if (entry.sectionInformation && entry.sectionInformation.sectionIdWithTerm) {
+            return entry.sectionInformation.sectionIdWithTerm;
+        }
+        return entry["Registered Section"];
+    }));
     const baseName = Array.from(sections).join("_");
     return baseName ? `penn_class_list_data_${baseName}.json` : "penn_class_list_data.json";
 }
 
-
-// **************************************************************************
-// MESSAGE LISTENER
-// **************************************************************************
 
 // **************************************************************************
 // MESSAGE LISTENER
