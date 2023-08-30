449858ba4e80ecf2532f326295a4a375530cd0dc
Jérémie Lumbroso <lumbroso@cs.princeton.edu>
2023-08-30 14:52:43 -0400

    Initial version of code

--- penn-classlist-scraper/background.js
+++ penn-classlist-scraper/background.js
@@ -1,8 +1,31 @@
-// Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
-    // No tabs or host permissions needed!
-    console.log("Applying Admiral Sucks! removal to: " + tab.url);
-    chrome.tabs.executeScript({
-        file: "content.js"
-    });
+    // Check if the URL matches the expected base URL
+    if (tab.url.startsWith("https://hosted.apps.upenn.edu/PennantReports/ClassListInstructor.aspx")) {
+        console.log("Calling Penn Class List Scraper on: " + tab.url);
+        chrome.tabs.executeScript({
+            file: "content.js"
+        });
+    } else {
+        console.warn("The current URL does not match the expected base URL for the Penn Class List Scraper.");
+    }
+});
+
+// Generate a filename based on unique sections
+function generateFilename(data) {
+    const sections = new Set(data.map(entry => entry["Registered Section"]));
+    const baseName = Array.from(sections).join("_");
+    return baseName ? `penn_class_list_data_${baseName}.json` : "penn_class_list_data.json";
+}
+
+// Listen for messages from content.js
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.action === "downloadJSON" && request.data) {
+        const blob = new Blob([JSON.stringify(request.data, null, 4)], {type: "application/json"});
+        const url = URL.createObjectURL(blob);
+        const filename = generateFilename(request.data);
+        chrome.downloads.download({
+            url: url,
+            filename: filename
+        });
+    }
 });
--- penn-classlist-scraper/content.js
+++ penn-classlist-scraper/content.js
@@ -1,42 +1,133 @@
-var htmls = Array.from(document.getElementsByTagName("html"));
-var bodys = Array.from(document.getElementsByTagName("body"));
-var divs = Array.from(document.getElementsByTagName("div"));
+// **************************************************************************
+// HELPER CODE
+// **************************************************************************
 
-var tags = htmls.concat(bodys, divs);
+// Function to extract data from the table row
+function extractDataFromRow(row) {
+    const data = {};
 
-for (var i=0; i < tags.length; i+= 1) {
-    var t = tags[i];
-    var cs = window.getComputedStyle(t);
-    if (t.style.zIndex > 100 || cs.zIndex > 100) {
-        t.style = "";
-        t.style.display = "none";
+    // Extract image link and student ID
+    const imgElement = row.querySelector('.pdfImage');
+    if (imgElement) {
+        data.imageLink = imgElement.src;
+        const studentIdMatch = imgElement.alt.match(/(\d+)/);
+        if (studentIdMatch) {
+            data.studentId = studentIdMatch[1];
+        }
     }
-    
-    t.style.position = "";
 
-    if (cs.overflow == "hidden") 
-        t.style.overflow = "scroll";
+    // Extract other details
+    const detailsCell = row.querySelector('td.TableRecords_EvenLine:not(.pdfImageTd)');
+    if (detailsCell) {
+        const detailsHtml = detailsCell.innerHTML.split('<br>');
+        detailsHtml.forEach(detail => {
+            const match = detail.match(/<b>([^:]+): <\/b>(.*)/);
+            if (match) {
+                const key = match[1].trim();
+                let value = match[2].trim();
+                if (key === 'EmailAddress' && value.includes('mailto:')) {
+                    value = value.match(/<a href="mailto:([^"]+)">/)[1];
+                }
+                data[key] = value;
+            }
+        });
+    }
+
+    // Split Name into Last and First, if Name exists
+    if (data.Name && typeof data.Name === 'string') {
+        const nameParts = data.Name.split(',');
+        data.Last = nameParts[0]?.trim();
+        data.First = nameParts[1]?.trim();
+    }
+
+    // Convert Privacy to boolean, if Privacy exists
+    if (data.Privacy) {
+        data.Privacy = data.Privacy === 'Y';
+    }
+
+    // Parse Advisors as a list, if Advisor exists
+    if (data.Advisor && typeof data.Advisor === 'string') {
+        data.Advisor = data.Advisor.split(';').map(advisor => advisor.trim());
+    }
+
+    // Split Primary Major and Primary Division, if they exist
+    if (data['Primary Major'] && typeof data['Primary Major'] === 'string') {
+        const majorMatch = data['Primary Major'].match(/(\w+) \(([^)]+)\)/);
+        if (majorMatch) {
+            data['Primary Major'] = majorMatch[1];
+            data['Primary Major Title'] = majorMatch[2];
+        }
+    }
+
+    if (data['Primary Division'] && typeof data['Primary Division'] === 'string') {
+        const divisionMatch = data['Primary Division'].match(/(\w+) \(([^)]+)\)/);
+        if (divisionMatch) {
+            data['Primary Division'] = divisionMatch[1];
+            data['Primary Division Title'] = divisionMatch[2];
+        }
+    }
+
+    return data;
+}
+
+// Function to fetch and encode image in base64
+// This function is separate from extractDataFromRow to maintain a clear separation of concerns.
+// extractDataFromRow is designed to extract data from the DOM, while this function deals with 
+// asynchronous network requests and blob manipulation.
+async function fetchAndEncodeImage(imageLink) {
+    try {
+        const response = await fetch(imageLink);
+        const blob = await response.blob();
+        return new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                resolve(reader.result);
+            };
+            reader.onerror = reject;
+            reader.readAsDataURL(blob);
+        });
+    } catch (error) {
+        console.warn('Failed to fetch or encode image:', imageLink);
+        return null; // Return null for failed image fetches
+    }
+}
+
+// Function to fetch all records
+async function fetchAllRecords() {
+    const tableRows = document.querySelectorAll('.pdfClassListEntry');
+    const allDataPromises = Array.from(tableRows).map(async row => {
+        try {
+            const data = extractDataFromRow(row);
+            data.imageBlob = await fetchAndEncodeImage(data.imageLink);
+            return data;
+        } catch (error) {
+            console.error('Error processing row:', row, error);
+            return null; // Return null for rows that encountered errors
+        }
+    });
+
+    const allData = await Promise.all(allDataPromises);
+    return allData.filter(data => data !== null); // Filter out null entries
 }
 
 
-// for hard-core scroll blockers
+// **************************************************************************
+// MAIN CODE
+// **************************************************************************
 
-for(var i = 0; i < document.styleSheets.length; i++) {
-    var classes = null;
-    try {
-        classes = document.styleSheets[i].rules || document.styleSheets[i].cssRules;
-    } catch (e) {
-        console.log("Can't read the css rules of: " + document.styleSheets[i].href, e);
-        continue;
-    }
-    for(var j = 0; j < document.styleSheets[i].rules.length; j++) {
-        if (document.styleSheets[i].rules[j].style) {
-            if (document.styleSheets[i].rules[j].style.overflow == "hidden") {
-                var selectorText = document.styleSheets[i].rules[j].selectorText;
-                //console.log(i + " " + j);
-                //console.log(document.styleSheets[i].rules[j].selectorText);
-                document.styleSheets[i].rules[j].style.overflow = "scroll";
-            }
-        }
-    }
+// Check if there are any elements with class ".pdfClassListEntry"
+const classListEntries = document.querySelectorAll('.pdfClassListEntry');
+if (classListEntries.length === 0) {
+    console.warn("No class list data found. Please ensure a proper search has been launched.");
+} else {
+    // Fetch all records and log the result
+    fetchAllRecords().then(allData => {
+        // Send the data back to background.js to trigger the download
+        chrome.runtime.sendMessage({
+            action: "downloadJSON",
+            data: allData
+        });
+    }).catch(error => {
+        console.error('Error fetching all records:', error);
+    });
 }
